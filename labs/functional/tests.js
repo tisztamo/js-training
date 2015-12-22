5c42fd739aa716147c110931ede3e91240b7d53a
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2015-12-22 11:51:15 +0100

    render() should do simple data binding with [data-view] on the children of the template fail

--- labs/functional/tests.js
+++ labs/functional/tests.js
@@ -1,16 +1,38 @@
 /*globals render*/
 "use strict";
 
-QUnit.test("render() should return an element with the same tagName as the template", function (assert) {
-  var tagName = "UL";
-  var template = document.createElement(tagName);
-  var rendered = render(template, {});
-  assert.ok(rendered.tagName === tagName);
-});
+(function testRender() {
+  QUnit.test("render() should return an element with the same tagName as the template", function (assert) {
+    var tagName = "UL";
+    var template = document.createElement(tagName);
+    var rendered = render(template, {});
+    assert.ok(rendered.tagName === tagName);
+  });
 
-QUnit.test("render() should return a new element with a different id", function (assert) {
-  var tagName = "UL";
-  var template = document.createElement(tagName);
-  var rendered = render(template, {});
-  assert.ok(rendered.id != template.id);
-});
+  QUnit.test("render() should return a new element with a different id", function (assert) {
+    var tagName = "UL";
+    var template = document.createElement(tagName);
+    var rendered = render(template, {});
+    assert.ok(rendered.id != template.id);
+  });
+
+  function createSimpleTemplate() {
+    var template = document.createElement("SPAN");
+    template.innerHTML = "<span data-view=\"title\"></span>";
+    return template;
+  }
+  
+  function createSimpleData() {
+    return {
+      title: "video1"
+    };
+  }
+
+  QUnit.test("render() should do simple data binding with [data-view] on the children of the template", function (assert) {
+    var template = createSimpleTemplate();
+    var data = createSimpleData();
+    var rendered = render(template, data);
+    assert.ok(rendered.querySelector("[data-view=title]").textContent == data.title);
+  });
+
+})();
