dd32d1f0715352fe34bf6167f141d431dc862beb
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2016-01-26 17:19:26 +0100

    oop lab

---
+++ labs/oop/model/spaceobject.js
@@ -0,0 +1,70 @@
+"use strict";
+
+/** Base class for simulation entities.
+ * @param pos {Vector} - Position of the object
+ * @param v {Vector} - Speed
+ * @param {number} [mass=1]
+ * @param {number} [heading=0]
+ * @param {number} [angularSpeed=0] - Speed of the rotation around the center of the object.
+ * @class
+ */
+function SpaceObject(pos, v, mass, heading, angularSpeed) {
+  this.pos = pos;
+  this.v = v;
+  this.mass = mass || 1;
+  this.heading = heading || 0;
+  this.angularSpeed = angularSpeed || 0;
+  /** The collected forces acting on the object in the current simulation step. Acting forces should be added to it.
+   * @type {Vector}
+   */
+  this.stepForce = new Vector(0, 0);
+  /** Radius of the circle used to approximate the shape of the object in basic collision-like calculations. For circular objects it is the radius of the object */
+  this.radius = 20;
+  this.id = SpaceObject.getNextId();
+  this.simulation = null;
+}
+
+/** Gravitational constant {@link https://en.wikipedia.org/wiki/Gravitational_constant} */
+SpaceObject.G = 50;
+
+/** simulates the gravity force between the two {@link SpaceObject}s. Does nothing when the distance is too small thus the high gravity force would lead to unacceptable calculation errors.
+ * @return {number} The distance of the given SpaceObjects
+ */
+SpaceObject.actGravityForce = function (spaceObject1, spaceObject2) {
+  var distance = spaceObject1.pos.distanceFrom(spaceObject2.pos);
+  if (distance < 15) { //Minimal distance needed
+    return;
+  }
+  var forceMagnitude = spaceObject1.mass * spaceObject2.mass * SpaceObject.G / Math.pow(distance, 2);
+  var forceDirection = spaceObject1.pos.substractToNew(spaceObject2.pos).toUnitVector();
+  var force = forceDirection.multiply(forceMagnitude);
+  spaceObject2.stepForce.add(force);
+  spaceObject1.stepForce.add(force.multiply(-1));
+  return distance;
+};
+
+/* @private */
+SpaceObject.nextId = 1;
+
+/* @private */
+SpaceObject.getNextId = function () {
+  var id = "SO" + SpaceObject.nextId;
+  SpaceObject.nextId += 1;
+  return id;
+};
+
+/**
+ * Simulates one step of this SpaceObject.
+ * Calculates the acceleration of the object from {@link SpaceObject#stepForce} and {@link SpaceObject#mass}. Also simulates the heading and position change.
+ * Should be extended in subclasses.
+ */
+SpaceObject.prototype.oneStep = function () {
+  this.v.add(this.stepForce.multiply(1 / this.mass));
+  this.pos.add(this.v);
+  this.heading += this.angularSpeed;
+  this.stepForce = new Vector(0, 0);
+};
+
+SpaceObject.prototype.toString = function() {
+  return "" + this.constructor.name;
+};
